2bd95ac2870a50aff7b4cb5f5757b633c87b1179
JohnWL <34081873+John-WL@users.noreply.github.com>
2022-12-11 00:22:00 -0500

    simplified text parsing

--- src/xml_prompt_transpiler.py
+++ src/xml_prompt_transpiler.py
@@ -3,44 +3,42 @@ import extensions.promptlang.src.ast_nodes as ast
 
 
 def transpile_prompt(prompt, steps):
-    root = xml_parser.fromstring("<prompt>\n" + prompt + "\n</prompt>")
+    root = xml_parser.fromstring('<prompt>\n' + prompt + '\n</prompt>')
     expression = xml_to_ast(root, steps)
     return expression.evaluate((0, steps))
 
 
 def xml_to_ast(node, steps):
     res = []
-
-    if node.text is not None:
-        text = node.text.strip()
-        if text != "":
-            res.append(ast.TextExpression(text))
+    
+    text = (node.text or '').strip()
+    if text != '':
+        res.append(ast.TextExpression(text))
 
     for child in node:
-        if child.tag == "range":
-            start = int(child.get('start') or "0")
+        if child.tag == 'range':
+            start = int(child.get('start') or '0')
             end = int(child.get('end') or str(steps))
             res.append(ast.RangeExpression(xml_to_ast(child, steps), start, end))
 
-        if child.tag == "weight":
-            weight = float(child.get('value') or "1")
+        if child.tag == 'weight':
+            weight = float(child.get('value') or '1')
             res.append(ast.WeightedExpression(xml_to_ast(child, steps), weight))
 
-        if child.tag == "weight_interpolation":
-            start = float(child.get('start') or "1")
-            end = float(child.get('end') or "1")
+        if child.tag == 'weight_interpolation':
+            start = float(child.get('start') or '1')
+            end = float(child.get('end') or '1')
             res.append(ast.WeightInterpolationExpression(xml_to_ast(child, steps), start, end))
 
-        if child.tail is not None:
-            text = child.tail.strip()
-            if text != "":
-                res.append(ast.TextExpression(text))
+        text = (child.tail or '').strip()
+        if text != '':
+            res.append(ast.TextExpression(text))
         xml_to_ast(child, steps)
     return ast.ListExpression(res)
 
 
-if __name__ == "__main__":
-    print(transpile_prompt("""
+if __name__ == '__main__':
+    print(transpile_prompt('''
 <weight value="1.2">
         extremely cute, beautiful and delicate 
         <range start="0" end="7">1girl, solo</range>
@@ -63,4 +61,4 @@ caustics,
 <weight value="1.1">
         wearing a japanese hanbok
 </weight>
-""", 30))
+''', 30))
