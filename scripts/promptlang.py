4adc0b437de675e794fba79a4865415fc7deedf5
Louis-Jacob Lebel <lebel.louisjacob@gmail.com>
2022-12-11 17:37:57 -0500

    fix infotext

--- scripts/promptlang.py
+++ scripts/promptlang.py
@@ -1,4 +1,6 @@
 import modules.scripts as scripts
+from modules.processing import create_infotext
+from modules.sd_hijack import model_hijack
 
 import sys
 base_dir = scripts.basedir()
@@ -22,6 +24,21 @@ class PromptlangScript(scripts.Script):
             for i, prompt in enumerate(prompts):
                 prompts[i] = transpile(prompt, p.steps)
 
+    def postprocess(self, p, processed, *args):
+        comments = {}
+        if len(model_hijack.comments) > 0:
+            for comment in model_hijack.comments:
+                comments[comment] = 1
+
+        def infotext(iteration=0, position_in_batch=0):
+            return create_infotext(p, p.all_prompts, p.all_seeds, p.all_subseeds, comments, iteration, position_in_batch)
+
+        for i, image in enumerate(processed.images):
+            batch_count = len(processed.images) / p.n_iter
+            print((i % batch_count, i // batch_count))
+            text = infotext(i % batch_count, i // batch_count)
+            image.info["parameters"] = text
+            processed.infotexts[i] = text
 
     def run(self, p, *args):
         pass
