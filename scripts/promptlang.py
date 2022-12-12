58c4ff48b97a606aad10d9e190a73558fe1332f0
ljleb <lebel.louisjacob@gmail.com>
2022-12-11 20:26:04 -0500

    abcdefg (#6)

--- scripts/promptlang.py
+++ scripts/promptlang.py
@@ -9,7 +9,7 @@ from lib.prompt_transpiler import transpile_prompt as transpile
 
 class PromptlangScript(scripts.Script):
     def __init__(self):
-        self.original_prompts = []
+        self.prompts = {}
 
     def title(self):
         return "Promptlang"
@@ -17,7 +17,17 @@ class PromptlangScript(scripts.Script):
     def show(self, is_img2img):
         return scripts.AlwaysVisible
 
-    def postsprocess_batch(self, p):
+    def process_batch(self, p, prompts, **kwargs):
+        for i, prompt in enumerate(prompts):
+            transpiled_prompt = transpile(prompt, p.steps)
+            self.prompts[transpiled_prompt] = prompt
+            prompts[i] = transpiled_prompt
+
+    def postprocess_batch(self, p, **kwargs):
         for prompts in [p.all_prompts, p.all_negative_prompts]:
             for i, prompt in enumerate(prompts):
-                prompts[i] = transpile(prompt, p.steps)
+                if prompt in self.prompts:
+                    prompts[i] = self.prompts[prompt]
+
+    def postprocess(self, *args, **kwargs):
+        self.prompts = {}
