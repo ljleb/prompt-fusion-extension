f09770ef33926f4a9f76418564ea0767252c6304
JohnWL <34081873+John-WL@users.noreply.github.com>
2022-12-14 10:28:21 -0500

    bezier implementation

---
+++ lib/bezier.py
@@ -0,0 +1,39 @@
+import copy
+import numpy as np
+
+
+def compute_on_curve_with_points(t, control_points):
+    copied_control_points = copy.deepcopy(control_points)
+    return compute_casteljau(t, copied_control_points, len(copied_control_points))
+
+
+def compute_casteljau(t, cp_list, size):
+    for i in reversed(range(1, size)):
+        for j in range(i):
+            cp_list[j] = cp_list[j] + t*(cp_list[j+1] - cp_list[j])
+    return cp_list[0]
+
+
+if __name__ == "__main__":
+    import turtle as tr
+    size = 30
+    turtle_tool = tr.Turtle()
+    turtle_tool.speed(10)
+    turtle_tool.up()
+
+    points = np.array([[0., 0], [2, -3], [-2, 4]])*100
+
+    for point in points:
+        turtle_tool.goto(point)
+        turtle_tool.dot(5, "red")
+
+    for i in range(size):
+        t = i/size
+        point = compute_on_curve_with_points(t, points)
+        turtle_tool.goto(point)
+        turtle_tool.dot()
+        print(point)
+
+    turtle_tool.goto(100000, 100000)
+    turtle_tool.dot()
+    tr.done()
--- scripts/promptlang.py
+++ scripts/promptlang.py
@@ -1,10 +1,43 @@
 import modules.scripts as scripts
+from modules import prompt_parser
+import re
 
 import sys
 base_dir = scripts.basedir()
 sys.path.append(base_dir)
 
 from lib.prompt_transpiler import transpile_prompt as transpile
+from lib.bezier import compute_on_curve_with_points as compute_bezier
+
+
+saved_get_learned_conditioning = prompt_parser.get_learned_conditioning
+
+
+re_INTERPOLATE = re.compile(r'\bINTERPOLATE\b')
+
+
+def hijacked_get_learned_conditioning(model, prompts, steps):
+    res = []
+
+    for prompt in prompts:
+        control_points = []
+        subprompts = re_INTERPOLATE.split(prompt)
+        for subprompt in subprompts:
+            subconditioning = saved_get_learned_conditioning(model, [subprompt], steps)[0][0].cond
+            control_points.append(subconditioning)
+
+        cond_array = []
+        if steps > 1:
+            for i in range(steps):
+                cond_array.append(prompt_parser.ScheduledPromptConditioning(end_at_step=i, cond=compute_bezier(i/(steps-1), control_points)))
+        else:
+            cond_array.append(prompt_parser.ScheduledPromptConditioning(end_at_step=steps, cond=compute_bezier(0.5, control_points)))
+        res.append(cond_array)
+
+    return res
+
+
+prompt_parser.get_learned_conditioning = hijacked_get_learned_conditioning
 
 
 class PromptlangScript(scripts.Script):
