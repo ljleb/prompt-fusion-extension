9ff1085a1a6e878a6f9207157b1107c4e4481779
Louis-Jacob Lebel <lebel.louisjacob@gmail.com>
2022-12-14 14:10:41 -0500

    better bezier

--- lib/catmul.py
+++ lib/catmul.py
@@ -12,34 +12,32 @@ def compute_catmul(t, control_points):
         return linear_interpolation(t, control_points)
     else:
         target_curve = min(int(t * (len(control_points) - 1)), len(control_points) - 1)
-        g0 = control_points[target_curve - 1] if target_curve > 0 else control_points[0] + control_points[0] - control_points[1]
+        g0 = control_points[target_curve - 1] if target_curve > 0 else 2 * control_points[0] - control_points[1]
         cp0 = control_points[target_curve]
         cp1 = control_points[target_curve + 1]
-        g1 = control_points[target_curve + 2] if target_curve + 2 < len(control_points) else cp1 + cp1 - cp0
-        ip0 = (cp1 + g0) / 2
-        ip1 = (cp0 + g1) / 2
-        ip0 = cp0 + cp1 - ip0
-        ip1 = cp1 + cp0 - ip1
+        g1 = control_points[target_curve + 2] if target_curve + 2 < len(control_points) else 2 * cp1 - cp0
+        ip1 = cp1 + (cp0 - g1)/2
+        ip0 = cp0 + (cp1 - g0)/2
         return compute_bezier(math.fmod(t * (len(control_points) - 1), 1.), [cp0, ip0, ip1, cp1])
 
 
 if __name__ == '__main__':
     import turtle as tr
-    size = 30
+    size = 50
     turtle_tool = tr.Turtle()
     turtle_tool.speed(10)
     turtle_tool.up()
 
-    points = numpy.array([[0., 1.], [1, -1], [2, -3], [3, -1]])*100
+    points = numpy.array([[0., 1, 2], [1, -1, 4], [2, 2, 2], [2, -3, 2], [3, -1, 2]])*100
 
     for point in points:
-        turtle_tool.goto(point)
+        turtle_tool.goto(point[:-1])
         turtle_tool.dot(5, "red")
 
     for i in range(size):
         t = i/size
         point = compute_catmul(t, points)
-        turtle_tool.goto(point)
+        turtle_tool.goto(point[:-1])
         turtle_tool.dot()
         print(point)
 
