0b548c98df9c734f00a0b7ec992113516480fabc
ljleb <lebel.louisjacob@gmail.com>
2022-12-18 02:35:30 -0500

    use lark to parse (#10)
    * try smth
    
    * w subst impl
    
    * begin refactoring parser
    
    * parser start working
    
    * initial lark parser
    
    * reuse weight range classes
    
    * ref
    
    * ref
    
    * reorder
    
    * quotes
    
    * unused code
    
    * fix substitutions
    
    * quotes aaaaaaaa
    
    * fix parsser
    
    * number step negative

--- lib/dsl_prompt_transpiler.py
+++ lib/dsl_prompt_transpiler.py
@@ -1,183 +1,114 @@
-import lib.ast_nodes as ast
+import ast_nodes as ast
+from lark import lark, v_args, Transformer
 
 
-def make_strict_token_parser(characters):
-    return make_token_parser(characters, lambda s, cs: s is cs, loop=False)
+expression_grammar = r'''
+start: list_expr_opt
+?list_expr: expr+ -> list_expr
+?list_expr_opt: expr* -> list_expr
+
+?expr: substitution_expr
+    | definition_expr
+    | weight_range_expr
+    | steps_range_expr
+    | text_expr
+
+?weight_range_expr: "(" list_expr_opt ":" weight_range_weight ")" -> weight_expr
+?weight_range_weight: weight | range{weight}
+?weight: weight_num | substitution_expr -> flatten_opt
+weight_num: sign (FLOAT | INTEGER) -> float_expr
+
+?steps_range_expr: "[" steps_range_exprs steps_range_steps "]" -> range_expr
+?steps_range_steps: step ("," step)* -> flatten_list
+?steps_range_exprs: (list_expr_opt ":")+ -> flatten_list
+?step: (step_num | substitution_expr)? -> flatten_opt
+step_num: sign INTEGER -> step_expr
+
+?definition_expr: "$" SYMBOL "=" expr list_expr -> assignment_expr
+substitution_expr: "$" SYMBOL -> substitution_expr
+
+?text_expr: (TEXT | DIGIT | COMMA)+ -> text_expr
+TEXT: /[^\[\]():|=$\s\d\-+]/+
+
+COMMA: ","
+
+range{number}: range_number{number} "," range_number{number} -> flatten_list
+range_number{number}: number? -> flatten_opt
+
+sign: SIGN? -> flatten_opt
+SIGN: "-" | "+"
+%import common.DIGIT
+%import common.CNAME -> SYMBOL
+%import common.FLOAT
+%import common.INT -> INTEGER
+
+%import common.WS -> WHITESPACE
+%ignore WHITESPACE
+'''
 
 
-def make_token_parser(characters, matcher=lambda s, cs: s in cs, loop=True):
-    def parse_token(source):
-        if not source:
-            raise ValueError
-        index = 0
-        for i in range(len(source)):
-            if not matcher(source[i], characters):
-                if i == 0:
-                    raise ValueError
-                break
-            index = i
-            if not loop:
-                break
+class ExpressionTransformer(Transformer):
+    @v_args(inline=True)
+    def flatten_list(self, *args):
+        return args
 
-        next_prompt_begin = index
-        for i in range(index + 1, len(source)):
-            if source[i] not in set(' \n\r\t'):
-                break
-            next_prompt_begin = i
+    @v_args(inline=True)
+    def flatten_opt(self, arg=None):
+        return arg
 
-        return source[:index + 1], source[next_prompt_begin + 1:]
+    def step_expr(self, args):
+        args = [arg for arg in args if arg is not None]
 
-    return parse_token
+        # `step + 1` because original language is off by 1
+        return ast.LiftExpression(int(''.join(args)) + 1)
 
+    def float_expr(self, args):
+        args = filter(lambda arg: arg is not None, args)
+        return ast.LiftExpression(float(''.join(args)))
+
+    def list_expr(self, args):
+        return ast.ListExpression(args)
+
+    def weight_expr(self, args):
+        if type(args[1]) is tuple:
+            return ast.WeightInterpolationExpression(args[0], *args[1])
+        else:
+            return ast.WeightedExpression(*args)
+
+    def range_expr(self, args):
+        return ast.RangeExpression(*args)
+
+    def assignment_expr(self, args):
+        return ast.DeclarationExpression(*args)
+
+    def substitution_expr(self, args):
+        return ast.SubstitutionExpression(*args)
+
+    def text_expr(self, args):
+        return ast.LiftExpression(str(' '.join(args)))
+
+
+expr_parser = lark.Lark(expression_grammar, parser='lalr', transformer=ExpressionTransformer())
+parse_expression = expr_parser.parse
 
 def transpile_prompt(prompt, steps):
     expression, prompt = parse_expression(prompt.lstrip())
     return expression.evaluate((0, steps))
 
 
-def parse_expression(prompt):
-    return parse_alternator_expression(prompt)
-
-
-def parse_alternator_expression(prompt):
-    expressions = []
-    head_expression, prompt = parse_list_expression(prompt)
-    expressions.append(head_expression)
-    try:
-        while True:
-            _, prompt = parse_alternator_separator(prompt)
-            expression, prompt = parse_list_expression(prompt)
-            expressions.append(expression)
-    except ValueError:
-        if len(expressions) > 1:
-            return ast.AlternatorExpression(expressions), prompt
-        else:
-            return expressions[0], prompt
-
-
-parse_alternator_separator = make_strict_token_parser('|')
-
-
-def parse_list_expression(prompt):
-    expressions = []
-    try:
-        while True:
-            expression, prompt = parse_recursive_expression(prompt)
-            expressions.append(expression)
-    except ValueError:
-        return ast.ListExpression(expressions), prompt
-
-
-def parse_recursive_expression(prompt):
-    try:
-        return parse_declaration(prompt)
-    except ValueError:
-        expression, prompt = parse_atom_expression(prompt)
-        try:
-            return parse_range_expression(expression, prompt)
-        except ValueError:
-            pass
-        try:
-            return parse_weighted_expression(expression, prompt)
-        except ValueError:
-            pass
-        return expression, prompt
-
-
-def parse_declaration(prompt):
-    symbol, prompt = parse_substitution_expression(prompt)
-    _, prompt = parse_assignment_separator(prompt)
-    value, prompt = parse_atom_expression(prompt)
-    expression, prompt = parse_expression(prompt)
-    return ast.DeclarationExpression(symbol, value, expression), prompt
-
-
-parse_assignment_separator = make_strict_token_parser('=')
-
-
-def parse_range_expression(expression, prompt):
-    (range_begin, range_end), prompt = parse_range(prompt, int)
-    return ast.RangeExpression(expression, range_begin, range_end), prompt
-
-
-def parse_number(prompt, number_type):
-    number, prompt = parse_digits(prompt)
-    return number_type(number), prompt
-
-
-parse_digits = make_token_parser('0123456789.')
-
-
-def parse_weighted_expression(expression, prompt):
-    _, prompt = parse_weight_separator(prompt)
-    try:
-        weight, prompt = parse_number(prompt, float)
-        return ast.WeightedExpression(expression, weight), prompt
-    except ValueError:
-        pass
-    (weight_begin, weight_end), prompt = parse_range(prompt, float)
-    return ast.WeightInterpolationExpression(expression, weight_begin, weight_end), prompt
-
-
-parse_weight_separator = make_strict_token_parser(':')
-
-
-def parse_range(prompt, number_type):
-    _, prompt = parse_range_begin(prompt)
-    try:
-        range_begin, prompt = parse_number(prompt, number_type)
-    except ValueError:
-        range_begin = None
-
-    _, prompt = parse_range_separator(prompt)
-    try:
-        range_end, prompt = parse_number(prompt, number_type)
-    except ValueError:
-        range_end = None
-
-    _, prompt = parse_range_end(prompt)
-    return (range_begin, range_end), prompt
-
-
-parse_range_begin = make_strict_token_parser('[')
-parse_range_end = make_strict_token_parser(']')
-parse_range_separator = make_strict_token_parser(',')
-
-
-def parse_atom_expression(prompt):
-    try:
-        return parse_parentheses_expression(prompt)
-    except ValueError:
-        pass
-    try:
-        return parse_substitution_expression(prompt)
-    except ValueError:
-        text, prompt = parse_text(prompt)
-        return ast.TextExpression(text), prompt
-
-
-def parse_parentheses_expression(prompt):
-    _, prompt = parse_parenthesis_begin(prompt)
-    expression, prompt = parse_expression(prompt)
-    _, prompt = parse_parenthesis_end(prompt)
-    return expression, prompt
-
-
-parse_parenthesis_begin = make_strict_token_parser('(')
-parse_parenthesis_end = make_strict_token_parser(')')
-
-
-def parse_substitution_expression(prompt):
-    _, prompt = parse_substitution_begin(prompt)
-    symbol, prompt = parse_symbol(prompt)
-    return ast.SubstitutionExpression(symbol), prompt
-
-
-parse_substitution_begin = make_strict_token_parser('$')
-
-parse_text = make_token_parser(' \n\r\t:[]()|=', lambda s, cs: s not in cs)
-
-parse_symbol = make_token_parser(
-    'abcdefghijklmnopqrstuvwxyz' +
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
-    '_')
+if __name__ == '__main__':
+    for i, prompt in enumerate([
+        'some space separated text',
+        '(legacy weighted prompt:-2.1)',
+        'mixed (legacy weight:3.6) and text',
+        'legacy [range begin:0] thingy',
+        'legacy [range end::3] thingy',
+        'legacy [[nested range::3]:2] thingy',
+        'sugar [range:2,3] thingy',
+        'sugar [(weight interpolation:1,2):0,1] thingy',
+        'sugar [(weight interpolation:1,2):0,2] thingy',
+        'sugar [(weight interpolation:1,2):0,3] thingy',
+        'legacy [from:to:2] thingy',
+    ]):
+        for e in parse_expression(prompt).children:
+            print(str(i) + ':\t' + e.evaluate((0, 5)))
