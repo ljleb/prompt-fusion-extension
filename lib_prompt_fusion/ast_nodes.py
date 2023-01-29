0546cdce211352628eebd4171a837de9e0cbd644
ljleb <lebel.louisjacob@gmail.com>
2023-01-29 01:17:26 -0500

    dont context aware

--- lib_prompt_fusion/ast_nodes.py
+++ lib_prompt_fusion/ast_nodes.py
@@ -83,7 +83,7 @@ class EditingExpression:
     def extend_tensor(self, tensor_builder, steps_range, total_steps, context):
         step = _eval_float(self.__step, steps_range, total_steps, context)
         if 0 < step < 1:
-            step = steps_range[0] + step * (steps_range[1] - steps_range[0])
+            step *= total_steps
 
         step = int(step)
 
