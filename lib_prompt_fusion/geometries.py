5d8970ee337b04081ca5963e7fc4afb94f464981
ljleb <lebel.louisjacob@gmail.com>
2023-05-04 17:21:30 -0400

    refact

--- lib_prompt_fusion/geometries.py
+++ lib_prompt_fusion/geometries.py
@@ -15,17 +15,16 @@ def slerp_geometry(control_points, params: interpolation_tensor.InterpolationPar
 
     angle = math.acos(float(similarity)) / 2
 
-    t_curve = angle * (2 * params.t - 1)
-    t_curve = math.tan(t_curve) / math.tan(angle)
-    t_curve = (t_curve + 1) / 2
+    slerp_t = angle * (2 * params.t - 1)
+    slerp_t = math.tan(slerp_t) / math.tan(angle)
+    slerp_t = (slerp_t + 1) / 2
 
-    np1 = p1 / p1_norm * p0_norm
-    mid = p0 + (np1 - p0) * t_curve
-    mid = mid / torch.linalg.norm(mid) * (p0_norm + (p1_norm - p0_norm) * params.t)
+    normalized_p1 = p1 / p1_norm * p0_norm
+    slerp_p = p0 + (normalized_p1 - p0) * slerp_t
+    slerp_p = slerp_p / torch.linalg.norm(slerp_p) * (p0_norm + (p1_norm - p0_norm) * params.t)
 
-    lin = linear_geometry(control_points, params)
-    new_params = interpolation_tensor.InterpolationParams(params.slerp_scale, *params[1:])
-    return linear_geometry([lin, mid], new_params)
+    lerp_p = linear_geometry(control_points, params)
+    return lerp_p + (slerp_p - lerp_p) * params.slerp_scale
 
 
 def linear_geometry(control_points, params: interpolation_tensor.InterpolationParams):
